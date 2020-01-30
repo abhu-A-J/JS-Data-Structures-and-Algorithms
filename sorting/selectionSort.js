a6b85cd97a5b51ffe3eb466c9918f7911606f9b1
Abhushan <abhushanadhikarijoshi@gmail.com>
2020-01-30 19:39:07 +0530

    Correction Selection Sort

--- sorting/selectionSort.js
+++ sorting/selectionSort.js
@@ -1,18 +1,21 @@
 /*Write a function to implement selection sort */
 
 function selectionSort(arr) {
-    for (let i = 0; i < arr.length-1; i++){
+    for (let i = 0; i < arr.length; i++){
         let smallest = i;
         for (let j = i + 1; j < arr.length; j++){
             if (arr[j] < arr[smallest]) {
                 smallest = j;
             }
         }
-        arr[i] = arr[i] + arr[smallest];
-        arr[smallest] = arr[i] - arr[smallest];
-        arr[i] = arr[i] - arr[smallest];
+        if (i !== smallest) {
+            arr[i] = arr[i] + arr[smallest];
+            arr[smallest] = arr[i] - arr[smallest];
+            arr[i] = arr[i] - arr[smallest];
+       }
     }
     return arr;
 }
 
-console.log(selectionSort([0,10,-10,-11,1]))
+console.log(selectionSort([0, 10, -10, -11, 1]))
+console.log(selectionSort([1, 2, -1, 0, 54, 6]))
